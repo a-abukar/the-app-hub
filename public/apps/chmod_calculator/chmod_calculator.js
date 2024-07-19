3ed2ffc886924821aea01eead2b0fb9068eddb90
Abdurahman Abukar <abzabukar99@gmail.com>
2024-07-19 20:06:30 +0100

    test

--- public/apps/chmod_calculator/chmod_calculator.js
+++ public/apps/chmod_calculator/chmod_calculator.js
@@ -18,7 +18,7 @@ document.addEventListener('DOMContentLoaded', () => {
       public_execute: document.getElementById('public-execute').checked,
     };
 
-    fetch('/calculate-chmod', {
+    fetch('api/calculate-chmod', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
